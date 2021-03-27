f9f1767736b47d20ea2d2885a5f4010129a19ff1
miladiashe <dkdud0122@naver.com>
2021-03-27 17:32:08 +0900

    숙제

--- pen.java
+++ pen.java
@@ -1,1 +1,15 @@
-.
+/*펜을 추상화 */
+/**
+ * pen
+ */
+public class pen {
+    String color;
+    int prise;
+    
+    public void write(){
+        System.out.println("Pen : write()");
+    }
+    public void erase(){
+        System.out.println("Pen : erase()");
+    }
+}
